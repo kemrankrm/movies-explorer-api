3f68411c86f9ac6f3815061998943152ef4457f7
Kemran Karimov <kemran.karimov@gmail.com>
2023-03-05 14:39:27 +0600

    fix: auth middleware fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
+const { AuthorizationError } = require('../scripts/errors/AuthorizationError');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    throw new AuthorizationError('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (e) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    throw new AuthorizationError('Необходима авторизация');
   }
 
   req.user = payload;
